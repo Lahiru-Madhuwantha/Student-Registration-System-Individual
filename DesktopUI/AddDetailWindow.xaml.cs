1415a4e893e03bbfc1a3ded7368a8be18680b40b
Asus <Asus@LAPTOP-3B0H1TQ0>
2023-07-16 11:31:44 +0530

    Add project files.

---
+++ DesktopUI/AddDetailWindow.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace DesktopUI
+{
+    /// <summary>
+    /// Interaction logic for AddDetailWindow.xaml
+    /// </summary>
+    public partial class AddDetailWindow : Window
+    {
+        public AddDetailWindow(AddUserVM vm)
+        {
+            InitializeComponent();
+            DataContext = vm;
+            vm.CloseAction = () => Close();
+        }
+
+        private void Button_Click()
+        {
+
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            //MainWindow secondWindow = new MainWindow();
+            //secondWindow.Show();
+            //this.Close();
+        }
+    }
+}
---
+++ DesktopUI/MainWindowVm.cs
@@ -0,0 +1,109 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using DesktopUI.Model;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Media.Imaging;
+
+namespace DesktopUI
+{
+    public partial class MainWindowVm : ObservableObject
+    {
+        [ObservableProperty]
+        public ObservableCollection<User>users;
+        [ObservableProperty]
+        public User selectedUser = null;
+
+
+
+        public void CloseMainWindow()
+        {
+            Application.Current.MainWindow.Close();
+        }
+
+        [RelayCommand]
+        public void messeag()
+        {
+
+            MessageBox.Show($"{selectedUser.FirstName} GPA value must be between 0 and 4.", "Error");
+        }
+
+        [RelayCommand]
+        public void AddStudent()
+        {
+            var vm = new AddUserVM();
+            vm.title = "ADD USER";
+            AddDetailWindow window = new AddDetailWindow(vm);
+
+            window.ShowDialog();
+           
+
+            if (vm.Student.FirstName != null)
+            {
+                users.Add(vm.Student);
+            }
+        }
+        [RelayCommand]
+        public void Delete()
+        {
+            MessageBoxResult result = MessageBox.Show("Are you sure you want to delete this user?", "Confirmation", MessageBoxButton.YesNo, MessageBoxImage.Question);
+
+            if (selectedUser != null)
+            {
+                string name = selectedUser.FirstName;
+                users.Remove(selectedUser);
+                MessageBox.Show($"{name} is Deleted successfuly.", "DELETED \a ");
+
+            }
+            else
+            {
+                MessageBox.Show("Plese Select Student before Delete.", "Error");
+
+
+            }
+        }
+        [RelayCommand]
+        public void ExecuteEditStudentCommand()
+        {
+            if (selectedUser != null)
+            {
+                var vm = new AddUserVM(selectedUser);
+                vm.title = "EDIT USER";
+                var window = new AddDetailWindow(vm);
+
+                window.ShowDialog();
+
+
+                int index = users.IndexOf(selectedUser);
+                users.RemoveAt(index);
+                users.Insert(index, vm.Student);
+
+
+
+            }
+            else
+            {
+                MessageBox.Show("Please Select the student", "Warning!");
+            }
+        }
+
+        public MainWindowVm()
+        {
+            users = new ObservableCollection<User>();
+            BitmapImage image1 = new BitmapImage(new Uri("/Model/Images/1.png", UriKind.Relative));
+            users.Add(new User(24, "Lahiru", "Madhuwantha", "1999/04/22", image1));
+            BitmapImage image2 = new BitmapImage(new Uri("/Model/Images/2.png", UriKind.Relative));
+            users.Add(new User(22, "Tharindu", "Imalka", "2000/10/23", image2));
+            BitmapImage image3 = new BitmapImage(new Uri("/Model/Images/3.png", UriKind.Relative));
+            users.Add(new User(23, "Thanuja", "Gayashan", "1999/12/13", image3));
+            BitmapImage image4 = new BitmapImage(new Uri("/Model/Images/4.png", UriKind.Relative));
+            users.Add(new User(23, "Deshani", "Dananjana", "1999/05/13", image4));
+
+        }
+    }
+}
